52088f39830bf4677312a0cb6e6b1042ae353cee
fareedhulfarzana17 <fareedhulfarzana17@gmail.com>
2026-04-20 17:34:35 +0530

    Update project1.js

--- project1.js
+++ project1.js
@@ -3,6 +3,38 @@ let navitemsElement=document.querySelector(".navitems");
  let menuiconElement=document.querySelector(".menuicon");
   let section1=document.querySelector(".section-1")
  menuiconElement.addEventListener("click",()=>{ 
+let navitemsElement=document.querySelector(".navitems");
+ let menuiconElement=document.querySelector(".menuicon");
+  let bodyclassElement=document.body;
+  
+ menuiconElement.addEventListener("click",(e)=>{ 
+   e.stopPropagation();
+  
+  
+    navitemsElement.classList.toggle("openmenu");
+    let click=navitemsElement.classList.contains("openmenu");
+    if(click){
+      menuiconElement.style.color="white";
+     }
+     else{
+      
+    menuiconElement.style.color="black";
+     }
+   
+ 
+
+ })
+  bodyclassElement.addEventListener("click",()=>{
+     navitemsElement.classList.remove("openmenu");
+    menuiconElement.style.color="black"
+
+  })
+ 
+  
+ 
+
+ 
+
   menuiconElement.style.color="white"
   let click=navitemsElement.classList.contains("openmenu");
   
