978323032607ed4f13a7bc9e5e28cdf3abf3b354
fareedhulfarzana17 <fareedhulfarzana17@gmail.com>
2026-04-20 17:38:26 +0530

    Update project1.js

--- project1.js
+++ project1.js
@@ -1,10 +1,6 @@
 
 let navitemsElement=document.querySelector(".navitems");
  let menuiconElement=document.querySelector(".menuicon");
-  let section1=document.querySelector(".section-1")
- menuiconElement.addEventListener("click",()=>{ 
-let navitemsElement=document.querySelector(".navitems");
- let menuiconElement=document.querySelector(".menuicon");
   let bodyclassElement=document.body;
   
  menuiconElement.addEventListener("click",(e)=>{ 
@@ -25,30 +21,6 @@ let navitemsElement=document.querySelector(".navitems");
 
  })
   bodyclassElement.addEventListener("click",()=>{
-     navitemsElement.classList.remove("openmenu");
-    menuiconElement.style.color="black"
-
-  })
- 
-  
- 
-
- 
-
-  menuiconElement.style.color="white"
-  let click=navitemsElement.classList.contains("openmenu");
-  
-  if(click){
-    navitemsElement.classList.remove("openmenu");
-    menuiconElement.style.color="black";
-  }
-  else{
-    navitemsElement.classList.add("openmenu");
-
-  }
-
-  })
-  section1.addEventListener("click",()=>{
      navitemsElement.classList.remove("openmenu");
     menuiconElement.style.color="black"
 
